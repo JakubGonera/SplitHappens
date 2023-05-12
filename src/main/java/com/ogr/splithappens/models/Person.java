785061ec5eb77d14c5c87746a98f751c3041c1bf
Matra <mati.spamy@gmail.com>
2023-05-12 16:09:57 +0200

    Person.getBalance() - first battle (lost)

--- src/main/java/com/ogr/splithappens/models/Person.java
+++ src/main/java/com/ogr/splithappens/models/Person.java
@@ -1,16 +1,21 @@
 package com.ogr.splithappens.models;
 
+import com.ogr.splithappens.viewmodels.ViewModel;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Person implements IPerson {
     // TODO: implement person interface
 
     private final String name;
+//    private final ViewModel viewModel;
     private final int id;
 
     public Person(String name, int id){
         this.name = name;
         this.id = id;
+//        this.viewModel = viewModel
         System.out.println("creatad person: "+id+" "+name);
     }
     @Override
@@ -24,12 +29,27 @@ public class Person implements IPerson {
     }
 
     @Override
-    public int getBalance() {
+    public int getBalance(){
+//        var expenses = getDetailedBalances();
+//        int balance = 0;
+//        for(var e:expenses) balance += e.balance();
+//        return balance;
         return 0;
     }
 
     @Override
     public List<detailedBalance> getDetailedBalances() {
+//        List<detailedBalance> list = new ArrayList<>();
+//
+//        var expenses = viewModel.getExpensesList();
+//        expenses.getValue().forEach(x -> {
+//            if(x.getPayerID() == id)
+//                list.add(new detailedBalance(x.getTitle(), x.getID(), -x.getAmount()));
+//            else
+//                list.add(new detailedBalance(x.getTitle(), x.getID(), x.getAmount()));
+//        });
+//
+//        return list;
         return null;
     }
 }
--- src/main/java/com/ogr/splithappens/models/PersonsManager.java
+++ src/main/java/com/ogr/splithappens/models/PersonsManager.java
@@ -1,13 +1,20 @@
 package com.ogr.splithappens.models;
 
 
+import com.ogr.splithappens.viewmodels.ViewModel;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class PersonsManager implements IPersonsManager {
     // TODO: implement PersonsManager
 
+//    private final ViewModel viewModel;
     private List<IPerson> personList = new ArrayList<>();
+
+//    public PersonsManager(ViewModel viewModel){
+//        this.viewModel = viewModel;
+//    }
     @Override
     public List<IPerson> getPersons() {
         return personList;
